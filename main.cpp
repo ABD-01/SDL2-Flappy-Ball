76dd3355497563257481f22b80df80a9b8672b1a
ABD-01 <63636498+ABD-01@users.noreply.github.com>
2026-01-04 00:19:19 +0530

    Changes for emscripten

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@
 #include <string>
 #include <vector>
 
+#define MAIN_FUNCTION SDL_main
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#undef MAIN_FUNCTION
+#define MAIN_FUNCTION main
+#endif
+
 int SCREEN_WIDTH  = 800;
 int SCREEN_HEIGHT = 600;
 
@@ -36,7 +43,189 @@ void drawCircle(SDL_Renderer* renderer, int x, int y, int r, SDL_Color color)
     }
 }
 
-int SDL_main(int argc, char* argv[])
+SDL_Window*   window   = NULL;
+SDL_Renderer* renderer = NULL;
+
+Ball  ball         = {100.0f, SCREEN_HEIGHT / 2.0f, 0.0f, 15.0f, {255, 50, 50, 255}};
+float gravity      = 0.4f;
+float jumpStrength = -7.0f;
+
+std::vector<Pillar> pillars;
+float               pillarSpeed      = 4.0f;
+int                 pillarSpawnTimer = 0;
+int                 score            = 0;
+
+bool      quit = false;
+SDL_Event e;
+bool      gameOver = false;
+
+// Helper to draw a single 7-segment digit
+auto drawDigit = [](int digit, int x, int y, int size) {
+    // bitmask for segments: a, b, c, d, e, f, g
+    //      a
+    //    f   b
+    //      g
+    //    e   c
+    //      d
+    unsigned char segments[] = {
+        0x3F, // 0: abcdef
+        0x06, // 1: bc
+        0x5B, // 2: abdeg
+        0x4F, // 3: abcdg
+        0x66, // 4: bcfg
+        0x6D, // 5: acdfg
+        0x7D, // 6: acdefg
+        0x07, // 7: abc
+        0x7F, // 8: abcdefg
+        0x6F  // 9: abcdfg
+    };
+    unsigned char mask = segments[digit % 10];
+    int           t    = size / 5; // thickness
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    if (mask & 0x01) {
+        SDL_Rect r = {x, y, size, t};
+        SDL_RenderFillRect(renderer, &r);
+    } // a
+    if (mask & 0x02) {
+        SDL_Rect r = {x + size - t, y, t, size};
+        SDL_RenderFillRect(renderer, &r);
+    } // b
+    if (mask & 0x04) {
+        SDL_Rect r = {x + size - t, y + size, t, size};
+        SDL_RenderFillRect(renderer, &r);
+    } // c
+    if (mask & 0x08) {
+        SDL_Rect r = {x, y + 2 * size - t, size, t};
+        SDL_RenderFillRect(renderer, &r);
+    } // d
+    if (mask & 0x10) {
+        SDL_Rect r = {x, y + size, t, size};
+        SDL_RenderFillRect(renderer, &r);
+    } // e
+    if (mask & 0x20) {
+        SDL_Rect r = {x, y, t, size};
+        SDL_RenderFillRect(renderer, &r);
+    } // f
+    if (mask & 0x40) {
+        SDL_Rect r = {x, y + size - t / 2, size, t};
+        SDL_RenderFillRect(renderer, &r);
+    } // g
+};
+
+auto drawNumber = [](int num, int x, int y, int size) {
+    std::string s = std::to_string(num);
+    for (int i = 0; i < (int)s.length(); ++i) {
+        drawDigit(s[i] - '0', x + i * (size + size / 2), y, size);
+    }
+};
+
+void gameLoop()
+{
+
+    while (SDL_PollEvent(&e) != 0) {
+        if (e.type == SDL_QUIT) { quit = true; }
+
+        bool inputTriggered = false;
+
+        if (e.type == SDL_KEYDOWN) {
+            if (e.key.keysym.sym == SDLK_SPACE) { inputTriggered = true; }
+        }
+
+        if (e.type == SDL_FINGERDOWN) { inputTriggered = true; }
+
+        if (inputTriggered) {
+            if (gameOver) {
+                // Reset game
+                ball.y  = SCREEN_HEIGHT / 2.0f;
+                ball.vy = 0;
+                pillars.clear();
+                score    = 0;
+                gameOver = false;
+            } else {
+                ball.vy = jumpStrength;
+            }
+        }
+    }
+
+    if (!gameOver) {
+        // Update ball
+        ball.vy += gravity;
+        ball.y += ball.vy;
+
+        // Spawn pillars
+        if (pillarSpawnTimer-- <= 0) {
+#ifdef __ANDROID__
+            float gap = 200.0f + (rand() % 50);
+#else
+            float gap = 150.0f + (rand() % 50);
+#endif
+            float minTop = 50.0f;
+            float maxTop = SCREEN_HEIGHT - gap - 50.0f;
+            float topH   = minTop + (rand() % (int)(maxTop - minTop));
+            pillars.push_back({(float)SCREEN_WIDTH, 60.0f, topH, gap, false});
+#ifdef __ANDROID__
+            pillarSpawnTimer = 150;
+#else
+            pillarSpawnTimer = 100;
+#endif
+        }
+
+        // Update pillars
+        for (auto& p : pillars) {
+            p.x -= pillarSpeed;
+            if (!p.passed && p.x + p.width < ball.x) {
+                p.passed = true;
+                score++;
+            }
+        }
+
+        // Remove off-screen pillars
+        pillars.erase(std::remove_if(pillars.begin(), pillars.end(), [](Pillar const& p) { return p.x + p.width < 0; }), pillars.end());
+
+        // Collision detection
+        if (ball.y - ball.radius < 0 || ball.y + ball.radius > SCREEN_HEIGHT) { gameOver = true; }
+
+        for (auto const& p : pillars) {
+            // Simplified AABB for ball vs pillar rectangles
+            bool hitTop    = (ball.x + ball.radius > p.x && ball.x - ball.radius < p.x + p.width && ball.y - ball.radius < p.topHeight);
+            bool hitBottom = (ball.x + ball.radius > p.x && ball.x - ball.radius < p.x + p.width && ball.y + ball.radius > p.topHeight + p.gap);
+            if (hitTop || hitBottom) { gameOver = true; }
+        }
+    }
+
+    // Rendering
+    SDL_SetRenderDrawColor(renderer, 24, 24, 24, 255);
+    SDL_RenderClear(renderer);
+
+    // Draw pillars
+    SDL_SetRenderDrawColor(renderer, 50, 200, 50, 255);
+    for (auto const& p : pillars) {
+        SDL_Rect topRect    = {(int)p.x, 0, (int)p.width, (int)p.topHeight};
+        SDL_Rect bottomRect = {(int)p.x, (int)(p.topHeight + p.gap), (int)p.width, (int)(SCREEN_HEIGHT - (p.topHeight + p.gap))};
+        SDL_RenderFillRect(renderer, &topRect);
+        SDL_RenderFillRect(renderer, &bottomRect);
+    }
+
+    // Draw ball
+    drawCircle(renderer, (int)ball.x, (int)ball.y, (int)ball.radius, ball.color);
+
+    // Draw Score
+    drawNumber(score, SCREEN_WIDTH / 2 - 20, 30, 20);
+
+    if (gameOver) {
+        // Visual feedback for game over (just a color filter or something simple
+        // for now)
+        SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 100);
+        SDL_Rect fullScreen = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+        SDL_RenderFillRect(renderer, &fullScreen);
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
+int MAIN_FUNCTION(int argc, char* argv[])
 {
     (void)argc;
     (void)argv;
@@ -67,14 +256,14 @@ int SDL_main(int argc, char* argv[])
     flags |= SDL_WINDOW_FULLSCREEN;
 #endif
 
-    SDL_Window* window = SDL_CreateWindow("Flappy Ball", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, flags);
+    window = SDL_CreateWindow("Flappy Ball", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, flags);
     if (!window) {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer) {
         std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
@@ -82,182 +271,13 @@ int SDL_main(int argc, char* argv[])
         return 1;
     }
 
-    Ball  ball         = {100.0f, SCREEN_HEIGHT / 2.0f, 0.0f, 15.0f, {255, 50, 50, 255}};
-    float gravity      = 0.4f;
-    float jumpStrength = -7.0f;
-
-    std::vector<Pillar> pillars;
-    float               pillarSpeed      = 4.0f;
-    int                 pillarSpawnTimer = 0;
-    int                 score            = 0;
-
-    bool      quit = false;
-    SDL_Event e;
-    bool      gameOver = false;
-
-    // Helper to draw a single 7-segment digit
-    auto drawDigit = [&](int digit, int x, int y, int size) {
-        // bitmask for segments: a, b, c, d, e, f, g
-        //      a
-        //    f   b
-        //      g
-        //    e   c
-        //      d
-        unsigned char segments[] = {
-            0x3F, // 0: abcdef
-            0x06, // 1: bc
-            0x5B, // 2: abdeg
-            0x4F, // 3: abcdg
-            0x66, // 4: bcfg
-            0x6D, // 5: acdfg
-            0x7D, // 6: acdefg
-            0x07, // 7: abc
-            0x7F, // 8: abcdefg
-            0x6F  // 9: abcdfg
-        };
-        unsigned char mask = segments[digit % 10];
-        int           t    = size / 5; // thickness
-
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        if (mask & 0x01) {
-            SDL_Rect r = {x, y, size, t};
-            SDL_RenderFillRect(renderer, &r);
-        } // a
-        if (mask & 0x02) {
-            SDL_Rect r = {x + size - t, y, t, size};
-            SDL_RenderFillRect(renderer, &r);
-        } // b
-        if (mask & 0x04) {
-            SDL_Rect r = {x + size - t, y + size, t, size};
-            SDL_RenderFillRect(renderer, &r);
-        } // c
-        if (mask & 0x08) {
-            SDL_Rect r = {x, y + 2 * size - t, size, t};
-            SDL_RenderFillRect(renderer, &r);
-        } // d
-        if (mask & 0x10) {
-            SDL_Rect r = {x, y + size, t, size};
-            SDL_RenderFillRect(renderer, &r);
-        } // e
-        if (mask & 0x20) {
-            SDL_Rect r = {x, y, t, size};
-            SDL_RenderFillRect(renderer, &r);
-        } // f
-        if (mask & 0x40) {
-            SDL_Rect r = {x, y + size - t / 2, size, t};
-            SDL_RenderFillRect(renderer, &r);
-        } // g
-    };
-
-    auto drawNumber = [&](int num, int x, int y, int size) {
-        std::string s = std::to_string(num);
-        for (int i = 0; i < (int)s.length(); ++i) {
-            drawDigit(s[i] - '0', x + i * (size + size / 2), y, size);
-        }
-    };
-
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(gameLoop, 0, 1);
+#else
     while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) { quit = true; }
-
-            bool inputTriggered = false;
-
-            if (e.type == SDL_KEYDOWN) {
-                if (e.key.keysym.sym == SDLK_SPACE) { inputTriggered = true; }
-            }
-
-            if (e.type == SDL_FINGERDOWN) { inputTriggered = true; }
-
-            if (inputTriggered) {
-                if (gameOver) {
-                    // Reset game
-                    ball.y  = SCREEN_HEIGHT / 2.0f;
-                    ball.vy = 0;
-                    pillars.clear();
-                    score    = 0;
-                    gameOver = false;
-                } else {
-                    ball.vy = jumpStrength;
-                }
-            }
-        }
-
-        if (!gameOver) {
-            // Update ball
-            ball.vy += gravity;
-            ball.y += ball.vy;
-
-            // Spawn pillars
-            if (pillarSpawnTimer-- <= 0) {
-#ifdef __ANDROID__
-                float gap    = 200.0f + (rand() % 50);
-#else
-                float gap    = 150.0f + (rand() % 50);
-#endif
-                float minTop = 50.0f;
-                float maxTop = SCREEN_HEIGHT - gap - 50.0f;
-                float topH   = minTop + (rand() % (int)(maxTop - minTop));
-                pillars.push_back({(float)SCREEN_WIDTH, 60.0f, topH, gap, false});
-#ifdef __ANDROID__
-                pillarSpawnTimer = 150;
-#else
-                pillarSpawnTimer = 100;
-#endif
-            }
-
-            // Update pillars
-            for (auto& p : pillars) {
-                p.x -= pillarSpeed;
-                if (!p.passed && p.x + p.width < ball.x) {
-                    p.passed = true;
-                    score++;
-                }
-            }
-
-            // Remove off-screen pillars
-            pillars.erase(std::remove_if(pillars.begin(), pillars.end(), [](Pillar const& p) { return p.x + p.width < 0; }), pillars.end());
-
-            // Collision detection
-            if (ball.y - ball.radius < 0 || ball.y + ball.radius > SCREEN_HEIGHT) { gameOver = true; }
-
-            for (auto const& p : pillars) {
-                // Simplified AABB for ball vs pillar rectangles
-                bool hitTop    = (ball.x + ball.radius > p.x && ball.x - ball.radius < p.x + p.width && ball.y - ball.radius < p.topHeight);
-                bool hitBottom = (ball.x + ball.radius > p.x && ball.x - ball.radius < p.x + p.width && ball.y + ball.radius > p.topHeight + p.gap);
-                if (hitTop || hitBottom) { gameOver = true; }
-            }
-        }
-
-        // Rendering
-        SDL_SetRenderDrawColor(renderer, 24, 24, 24, 255);
-        SDL_RenderClear(renderer);
-
-        // Draw pillars
-        SDL_SetRenderDrawColor(renderer, 50, 200, 50, 255);
-        for (auto const& p : pillars) {
-            SDL_Rect topRect    = {(int)p.x, 0, (int)p.width, (int)p.topHeight};
-            SDL_Rect bottomRect = {(int)p.x, (int)(p.topHeight + p.gap), (int)p.width, (int)(SCREEN_HEIGHT - (p.topHeight + p.gap))};
-            SDL_RenderFillRect(renderer, &topRect);
-            SDL_RenderFillRect(renderer, &bottomRect);
-        }
-
-        // Draw ball
-        drawCircle(renderer, (int)ball.x, (int)ball.y, (int)ball.radius, ball.color);
-
-        // Draw Score
-        drawNumber(score, SCREEN_WIDTH / 2 - 20, 30, 20);
-
-        if (gameOver) {
-            // Visual feedback for game over (just a color filter or something simple
-            // for now)
-            SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 100);
-            SDL_Rect fullScreen = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
-            SDL_RenderFillRect(renderer, &fullScreen);
-        }
-
-        SDL_RenderPresent(renderer);
+        gameLoop();
     }
+#endif
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
